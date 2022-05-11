df8ec21bb07b3f7821b0ad1dae21c9ddffcd7936
Radser2001 <supuni.randeniya@gmail.com>
2022-05-11 12:42:38 +0530

    header and footer

--- include/footer.php
+++ include/footer.php
@@ -1,3 +1,4 @@
+<!--footer-->
 <footer>
     <p class="footer-text">Copyright &copy; 2022 bys Rader2001</p>
 </footer>
--- include/header.php
+++ include/header.php
@@ -1,10 +1,8 @@
+<!--header-->
 <header>
     <nav class="navbar">
         <div class="system-name">
             <h2>Student-Management-System</h2>
         </div>
-        <!-- <div class="links">
-            <button class="button">Sign in</button>
-        </div> -->
     </nav>
 </header>
